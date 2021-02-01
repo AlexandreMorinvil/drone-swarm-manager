14ef7d1fc12acddde7f7e2e4ace0b3a9210d3c86
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2021-02-01 16:12:57 -0500

    Create main pages routes for the client side of the application

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ControlPageComponent } from "./components/page/control/control-page.component";
+import { HomePageComponent } from "./components/page/home/home-page.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: HomePageComponent },
+  { path: "control", component: ControlPageComponent },
+  { path: "**", redirectTo: "/home" },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
