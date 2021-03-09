f99ae55701df46e09e1f8337c854a2d435e4292a
jdandjinou <julio-herve.dandjinou@polymtl.ca>
2021-03-09 16:18:17 -0500

    Select arg for lunch server

--- server/src/server.py
+++ server/src/server.py
@@ -4,6 +4,7 @@ from vec3 import Vec3
 from drone import *
 import threading
 import sys
+import argparse
 
 from flask import Flask, jsonify, render_template
 from flask_socketio import *
@@ -24,13 +25,18 @@ socketio = SocketIO(app ,cors_allowed_origins='*')
 
 default_port = 5015
 
-# Select mode
-if (sys.argv[1] == 'simulation'):
+# Select mode in commande line
+parser =  argparse.ArgumentParser()
+parser.add_argument('mode', default='realtime', choices=['simulation', 'realtime'], help='Type simulation for simulation mode and type realtime for real time mode')
+args = parser.parse_args()
+
+if (args.mode == 'simulation'):
     mode = Mode.SIMULATION
     print('mode simulation')
-else:
+elif (args.mode == 'realtime'):
     mode = Mode.REAL_TIME
     print('mode real time')
+    
 # Initialize the low-level drivers (don't list the debug drivers)
 cflib.crtp.init_drivers(enable_debug_driver=False)
 # Scan for Crazyflies and use the first one found
