f6d698d9a2670048cafc520ce203a08cd3926249
Alexandre Morinvil <alexandre.morinvil@outlook.com>
2021-03-11 22:07:39 -0500

    Asjust launch script

--- simulation/launch_scripts/launch_container.sh
+++ simulation/launch_scripts/launch_container.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+BASEDIR=$(dirname "$0")
 SIMULATION_CONTAINER_ID=$(docker ps -qf "name=simulation-argos")
 
 # Launch the container if it was not launched
 if [ -z $SIMULATION_CONTAINER_ID ]
 then
-    docker build -f ../Dockerfile .. --tag simulation-argos --network host
+    docker build -f $BASEDIR/../Dockerfile $BASEDIR/.. --tag simulation-argos --network host
     docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
     x11docker --hostdisplay --hostnet --user=RETAIN -- --privileged simulation-argos &
     sleep 10
