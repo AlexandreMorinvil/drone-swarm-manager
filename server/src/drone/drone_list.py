723e4a55e05b04a16573c4cec4232ccaadd14995
Alexandre <alexandre.talens@polymtl.ca>
2021-04-21 16:26:20 +0000

    Refactor/server

---
+++ server/src/drone/drone_list.py
@@ -0,0 +1,78 @@
+# Add paths toward dependecies in different subdirectories
+import os
+import sys
+sys.path.append(os.path.abspath('./log'))
+
+# Add dependencies
+import threading
+import json
+from setup_logging import LogsConfig
+from environment import Environment
+from drone_real import DroneReal
+from drone_simulation import DroneSimulation
+from drone_interface import DroneInterface, StateMode
+from vec3 import Vec3
+
+# Add dependecies
+
+# Singleton Drone list
+
+
+class DroneList:
+
+    # Singleton attributes
+    drones = []
+    initial_posisitions = []
+    default_port = 5015
+    is_drone_list_initialized = False
+
+    # Logger attribute
+    logsConfig = None
+    logger = None
+
+    def __init__(self, drone_list=[]):
+        if not DroneList.is_drone_list_initialized:
+            # Initialize the drone list
+            DroneList.drone_list = drone_list
+
+            # Initialize the logger
+            DroneList.logsConfig = LogsConfig()
+            DroneList.logger = self.logsConfig.logger('DroneList')
+
+            # Indicate the initialization as completed
+            DroneList.is_drone_list_initialized = True
+
+    @classmethod
+    def dumps(cls):
+        return json.dumps([drone.dump() for drone in cls.drones])
+
+    @classmethod
+    def createDrones(cls, number_drones, mode):
+        for i in range(number_drones):
+            if Environment.is_in_simulation():
+                cls.drones.append(DroneSimulation(cls.default_port + i))
+                t = threading.Thread(
+                    target=cls.drones[i].waiting_connection, name='waiting_connection')
+                t.start()
+                cls.logger.info(
+                    'Connection to port {}'.format(cls.default_port + i))
+            else:
+                initial_posisitions_vec3 = Vec3(DroneList.initial_posisitions[i]['x'], DroneList.initial_posisitions[i]['y'])
+                cls.drones.append(
+                    DroneReal(DroneList.initial_posisitions[i]['address'], initial_posisitions_vec3))
+
+    @classmethod
+    def delete_drones(cls):
+        """Disconnect the drones brefore resetting the list"""
+        for drone in cls.drones:
+            drone.delete()
+        cls.drones.clear()
+        cls.logger.info('Deleted all drones from the drone list')
+
+    @classmethod
+    def get_drone_from_id(cls, id):
+        return cls.drones[id - DroneInterface.id_counter]
+
+    @classmethod
+    def get_number_drones(cls):
+        return drones.length
--- server/src/map/map.py
+++ server/src/map/map.py
@@ -1,34 +1,55 @@
 from vec3 import Vec3
-import math
 import sys
 import os
+from datetime import datetime
 from DBconnect import DatabaseConnector
 
 # Add paths toward dependecies in different subdirectories
 sys.path.insert(1, os.path.abspath('../'))
 
+
 class Map:
-    def __init__(self, name, id):
-        self.db = DatabaseConnector()
-        self.__name = name
-        self.__points = []
+
+    RESOLUTION = 0.05
+
+    def __init__(self, id=-1, name=-1, time=datetime.now()):
         self.id = id
-        self._endMission = False
-        
+        self.name = name
+        self.time = time
+        self.__points = {}
 
     def addPoint(self, point):
-        self.__points.append(point)
+        # If the rounded point is present, we ignore the point
+        point_to_add = self.round_point(point)
+        if self.is_point_present(point_to_add):
+            return None
 
-    def setBaseMap(self, points, name="Map Name"):
-        self.__name = name
-        self.__points = points
+        # If the point is a new point, we add it to the map structure
+        if not point_to_add.x in self.__points:
+            self.__points[point_to_add.x] = set()  
+        self.__points[point_to_add.x].add(point_to_add.y)
+        
+        # Return the point saved
+        return point_to_add
 
-    def toJson(self):
+    def is_point_present(self, point):
+        # Verify whether or not the point is already present in the map
+        return point.x in self.__points and point.y in self.__points[point.x]
+
+    def get_points(self):
+        return [Vec3(row, column).to_json() for row in map for column in map[row]]
+
+    def round_point(self, point):
+        # Round the position of the point to the closest point which is a multiple of the RESOLUTION
+        rounded_point = point.mul(1/Map.RESOLUTION)
+        rounded_point = rounded_point.round()
+        rounded_point = rounded_point.mul(Map.RESOLUTION)
+        return rounded_point
+
+
+    def to_json(self):
+        points = self.get_points()
         return {
-            'name': self.__name,
-            'points': self.__points
+            name: self.__name,
+            points: [point.to_json() for point in points]
         }
-
-    def end_mission(self):
-        self.db.update_map(self.id, self.__databasePoint)
-
